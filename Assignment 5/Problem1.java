e9d34f71e679853eefb3ce21e0c09090530f5f31
JWilson45 <jason.wilson2@marist.edu>
2020-03-05 14:25:52 -0500

    Assignment 5 base

---
+++ Assignment 5/Problem1.java
@@ -0,0 +1,34 @@
+ 
+public class Problem1 {
+
+
+
+	public void findmaxdiff (double[] A)
+	{
+		// Complete the method to find the maximum difference between points in A[]
+		// so that the larger value appears after the smaller value in A[]
+		// feel free to change the return type, parameters
+		// full credits will be awarded to divide and conquer and O(n)
+
+	}
+
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// Test your findmaxdiff() method here
+
+		double[] testarray1 = {2, 3, 10, 6, 4, 8, 1};
+		// maxdiff: 8
+
+		double[] testarray2 = {7, 9, 1, 6, 3, 2};
+		// maxdiff: 5
+
+
+		// Add test statements
+
+
+
+	}
+}
---
+++ Assignment 5/Problem3.java
@@ -0,0 +1,32 @@
+public class Problem3 {
+	
+	
+	
+	
+	public static void commonelements(double[] s1, double[] s2)
+	{
+		// complete the commonelements() method to find out
+		// elements that occur in both s1 and s2
+		// feel free to change method type and parameters
+		// Full credit will awarded to algorithms O(n)
+	
+		
+		
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Test your commonelements() method here
+		double[] testarray1 = {0, 0, 0, 1, 2, 3, 97, 98};
+		double[] testarray2 = {0, 1, 2, 3, 4, 4, 10, 98, 100, 100};
+
+		
+		System.out.println("Elements that occur in both arrays: ");
+		commonelements(testarray1,testarray2);
+		//should output 0, 1, 2, 3, 98
+		
+	}
+
+}
